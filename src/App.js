30b616c9e300d2d15dfe9cf9ac31e99a57bbb066
arcinston <arushkurundodi@gmail.com>
2021-03-18 16:38:55 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Item from "./Item"
+import "./style.css"
+function App()
+{return (
+  <div className="list">
+    <Item />
+    <Item />
+    <Item />
+    <Item />
+  </div>
+);}
+
+export default App
---
+++ src/Item.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./style.css";
+
+function Item()
+{return(<div className="item">
+    <input type="checkbox" name="task1"/>
+    <label name="task1"> Task </label> 
+</div>)}
+export default Item
